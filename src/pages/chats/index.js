f4256e6c537ecb604380201d0213ed5b5efff20d
Maxim Balashov <79164211428@yandex.ru>
2022-02-06 14:15:16 +0300

    problem with file hash solved

--- src/pages/chats/index.js
+++ src/pages/chats/index.js
@@ -1,146 +1,128 @@
 console.log("loaded");
 import Handlebars from "handlebars";
 import { ModalWindow } from "../../components/modal-window/modalWindow.partial";
-import { Chat } from "./chat";
 import templateFunction from "./chats.hbs";
-// const string = document.getElementById("templ").innerHTML;
+import svgDefaultChatPic from "../../static/img/Chat-picture.svg";
+import svgSearch from "../../static/img/Search.svg";
+import svgMenu from "../../static/img/Menu.svg";
+import svgCreateChat from "../../static/img/Create-chat.svg";
+import svgDeleteChat from "../../static/img/Delete-chat.svg";
+import svgChangeChat from "../../static/img/Change-ava-chat.svg";
+import svgAddVideoOrFoto from "../../static/img/Video-or-foto.svg";
+import svgAddFile from "../../static/img/File.svg";
+import svgAddLocation from "../../static/img/Location.svg";
+import svgSendFile from "../../static/img/Image.svg";
+import svgArrowRight from "../../static/img/Arrow-right.svg";
 
 const root = document.getElementById("root");
 
 Handlebars.registerPartial("modalWindow", ModalWindow);
-// const template = Handlebars.compile(string);
-Handlebars.registerHelper("include", function (source) {
-  return new Handlebars.SafeString(source);
-});
+// Handlebars.registerHelper("include", function (source) {
+//   return new Handlebars.SafeString(source);
+// });
 const dataObj = {
+  svgDefault: {
+    svgDefaultChatPic,
+    svgSearch,
+    svgMenu,
+    svgCreateChat,
+    svgDeleteChat,
+    svgChangeChat,
+    svgAddVideoOrFoto,
+    svgAddFile,
+    svgAddLocation,
+    svgSendFile,
+    svgArrowRight,
+  },
   chats: [
     {
       name: "Andrey",
       time: "11:22",
       lastMessage: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      imageSrc: "./src/static/img/Chat-picture.svg",
+      imageSrc: svgDefaultChatPic,
       newMessageCount: 3,
     },
     {
       name: "Andrey",
       time: "11:33",
       lastMessage: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      imageSrc: "./Chat-picture.svg",
+      imageSrc: svgDefaultChatPic,
       newMessageCount: 3,
     },
     {
       name: "Andrey",
       time: "11:44",
       lastMessage: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      imageSrc: "../../static/img/Chat-picture.svg",
+      imageSrc: svgDefaultChatPic,
       newMessageCount: 4,
     },
     {
       name: "Andrey",
       time: "11:55",
       lastMessage: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      imageSrc: "../../static/img/Chat-picture.svg",
+      imageSrc: svgDefaultChatPic,
       newMessageCount: 25,
     },
     {
       name: "Andrey",
       time: "11:00",
       lastMessage: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      imageSrc: "../../static/img/Chat-picture.svg",
+      imageSrc: svgDefaultChatPic,
       newMessageCount: 6,
     },
     {
       name: "Andrey",
       time: "11:00",
       lastMessage: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      imageSrc: "../../static/img/Chat-picture.svg",
+      imageSrc: svgDefaultChatPic,
       newMessageCount: 6,
     },
     {
       name: "Andrey",
       time: "11:00",
       lastMessage: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      imageSrc: "../../static/img/Chat-picture.svg",
+      imageSrc: svgDefaultChatPic,
       newMessageCount: 6,
     },
     {
       name: "Andrey",
       time: "11:00",
       lastMessage: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      imageSrc: "../../static/img/Chat-picture.svg",
+      imageSrc: svgDefaultChatPic,
       newMessageCount: 6,
     },
     {
       name: "Andrey",
       time: "11:00",
       lastMessage: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      imageSrc: "../../static/img/Chat-picture.svg",
+      imageSrc: svgDefaultChatPic,
       newMessageCount: 6,
     },
     {
       name: "Andrey",
       time: "11:00",
       lastMessage: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      imageSrc: "../../static/img/Chat-picture.svg",
+      imageSrc: svgDefaultChatPic,
       newMessageCount: 6,
     },
     {
       name: "Andrey",
       time: "11:00",
       lastMessage: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      imageSrc: "../../static/img/Chat-picture.svg",
+      imageSrc: svgDefaultChatPic,
       newMessageCount: 6,
     },
     {
       name: "Andrey",
       time: "11:00",
       lastMessage: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      imageSrc: "../../static/img/Chat-picture.svg",
+      imageSrc: svgDefaultChatPic,
       newMessageCount: 6,
     },
   ],
 };
-const chats = [
-  {
-    name: "Andrey",
-    time: "11:22",
-    lastMessage: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    imageSrc: "../../static/img/Chat-picture.svg",
-    newMessageCount: 2,
-  },
-  {
-    name: "Andrey",
-    time: "11:33",
-    lastMessage: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    imageSrc: "../../static/img/Chat-picture.svg",
-    newMessageCount: 3,
-  },
-  {
-    name: "Andrey",
-    time: "11:44",
-    lastMessage: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    imageSrc: "../../static/img/Chat-picture.svg",
-    newMessageCount: 4,
-  },
-  {
-    name: "Andrey",
-    time: "11:55",
-    lastMessage: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    imageSrc: "../../static/img/Chat-picture.svg",
-    newMessageCount: 25,
-  },
-  {
-    name: "Andrey",
-    time: "11:00",
-    lastMessage: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    imageSrc: "../../static/img/Chat-picture.svg",
-    newMessageCount: 6,
-  },
-];
-document.body.innerHTML = templateFunction(dataObj);
 
-const html = template(dataObj);
-root.innerHTML = html;
+root.innerHTML = templateFunction(dataObj);
 
 // Handlebars.registerPartial("templ", partial);
 // Handlebars.registerPartial("footer", footer);

b904120e8274076157f39e107e5a01452d6f5112
Maxim Balashov <79164211428@yandex.ru>
2022-03-29 23:53:58 +0300

    some changes

--- src/controllers/LoaderController.ts
+++ src/controllers/LoaderController.ts
@@ -1,17 +1,17 @@
 import store from "../modules/Store/Store";
 
 class LoaderConteroller {
-    store: typeof store;
-    constructor() {
-        this.store = store;
-    }
-    start(){
-        this.store.set('isLoading', true)
-    }
-    end(){
-        this.store.set('isLoading', false)
+  store: typeof store;
+  constructor() {
+    this.store = store;
+  }
+  start(){
+    this.store.set("isLoading", true);
+  }
+  end(){
+    this.store.set("isLoading", false);
 
-    }
+  }
 }
 //adapterForUserData add '_'
 export default new LoaderConteroller();
